f83cf92367b977678eceddced23537bffe6ca1b5
Joshua Winkler <josh@bemoty.dev>
2022-09-05 01:51:13 +0200

    fix: val

--- src/commands/QuoteCommand.ts
+++ src/commands/QuoteCommand.ts
@@ -5,7 +5,11 @@ import Quote from '../models/Quote'
 export default class QuoteCommand implements ICommand {
     async execute(interaction: CommandInteraction<CacheType>): Promise<void> {
         let quote = interaction.options.get('quote')
-        let stringQuote = (quote as string).replaceAll('\\n', '\n')
+        if (!quote) {
+            await interaction.reply({ content: 'You need to provide a quote!', ephemeral: true })
+            return
+        }
+        let stringQuote = (quote.value as string).replaceAll('\\n', '\n')
         const quoteObject = new Quote(stringQuote, interaction.user.id, Date.now())
         const quoteCreator = interaction.guild?.members.cache.find((member) => member.id === interaction.user.id)
         await quoteObject.create() // this usually never takes more than 3 seconds, so we don't need to defer
