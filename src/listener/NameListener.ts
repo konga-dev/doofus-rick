6b420af99475734f0c7fb5f94f6c7178badf80ec
Joshua Winkler <josh@bemoty.dev>
2023-10-20 11:08:43 +0200

    remove namelistener

--- src/listener/NameListener.ts
+++ src/listener/NameListener.ts
@@ -1,13 +1,18 @@
 import { GuildMember, PartialGuildMember } from 'discord.js'
 
-const CLIENT_ID = '155046312411267072'
+const CLIENT_ID = '275342581821603842'
+
+const MIN_TIME_MIN = 120
+const MAX_TIME_MIN = 2500
 
 const onEvent = async (oldMember: PartialGuildMember | GuildMember, newMember: GuildMember) => {
     if (newMember.id !== CLIENT_ID) {
         return
     }
-    if (newMember.displayName !== '!Josh') {
-        await newMember.setNickname('!Josh')
+    if (newMember.displayName !== 'nicolaus') {
+        setTimeout(async () => {
+            await newMember.setNickname('nicolaus')
+        }, Math.floor(Math.random() * (MAX_TIME_MIN - MIN_TIME_MIN + 1) + MIN_TIME_MIN) * 60 * 1000)
     }
 }
 
