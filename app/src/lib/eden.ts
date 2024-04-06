132527c770a11fef7b9f02e43ddb4957f7cd3233
Joshua Winkler <josh@bemoty.dev>
2024-04-06 20:17:07 +0200

    chore: use docker hostname

--- app/src/lib/eden.ts
+++ app/src/lib/eden.ts
@@ -2,7 +2,7 @@ import { dev } from '$app/environment'
 import { edenFetch, edenTreaty } from '@elysiajs/eden'
 import type { Server } from '../../../server/src/elysia/Server'
 
-const target = dev ? 'http://localhost:3000' : 'https://doofus-rick-api.josholaus.com'
+const target = dev ? 'http://localhost:3000' : 'http://doofus-rick:3000'
 
 export const treaty = edenTreaty<Server>(target)
 
