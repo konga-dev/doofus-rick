50b15c14276538f75531021c4f1f1dd9ccb8e100
Joshua Winkler <josh@bemoty.dev>
2024-04-06 19:58:15 +0200

    chore: split images

--- app/src/lib/eden.ts
+++ app/src/lib/eden.ts
@@ -1,8 +1,8 @@
+import { dev } from '$app/environment'
 import { edenFetch, edenTreaty } from '@elysiajs/eden'
 import type { Server } from '../../../server/src/elysia/Server'
 
-const target =
-    process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://doofus-rick-api.josholaus.com'
+const target = dev ? 'http://localhost:3000' : 'https://doofus-rick-api.josholaus.com'
 
 export const treaty = edenTreaty<Server>(target)
 
