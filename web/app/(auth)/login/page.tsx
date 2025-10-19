d10e019b754fe2d89f45b3f494fe4cac66d0f44d
Joshua Winkler <josh@bemoty.dev>
2025-10-19 16:59:18 +0200

    fix: i'm losing it

--- web/app/(auth)/login/page.tsx
+++ web/app/(auth)/login/page.tsx
@@ -6,6 +6,7 @@ import SignInButton from "@/components/SignInButton";
 import {checkAccess} from "@/lib/auth-client";
 import {cookies, headers} from "next/headers";
 import {redirect} from "next/navigation";
+import ForceReload from "@/components/ForceReload";
 
 export const dynamic = 'force-dynamic'
 
@@ -17,56 +18,59 @@ export default async function SignIn() {
         redirect('/')
     }
 
-	return (
-		<Card className="w-full">
-			<CardHeader>
-				<div className="flex flex-row items-center gap-2 mt-4">
-					<Avatar className="h-24 w-24 rounded-full mr-2">
-						<AvatarImage src="/doofus-rick.png" alt="CN" />
-						<AvatarFallback />
-					</Avatar>
-					<div className="flex flex-col">
-						<div className="text-2xl font-bold">doofus rick</div>
-						<div className="italic">
-							cause it can't get any worse than this
-						</div>
-					</div>
-				</div>
-				<CardTitle className="mt-4 text-lg md:text-xl">
-					Sign In
-				</CardTitle>
-				<CardDescription className="text-xs md:text-sm">
-					Use your Discord account to login
-				</CardDescription>
-			</CardHeader>
-			<CardContent>
-				<div className="grid gap-4">
-					<div
-						className={cn(
-							'w-full gap-2 flex items-center',
-							'justify-between flex-col',
-						)}
-					>
-                        <SignInButton/>
-					</div>
-				</div>
-			</CardContent>
-			<CardFooter>
-				<div className="flex justify-center w-full border-t py-4">
-					<p className="text-center text-xs text-neutral-500">
-						built by{' '}
-						<Link
-							href="https://konga.dev"
-							className="underline"
-							target="_blank"
-						>
+    return (
+        <>
+            <ForceReload/>
+            <Card className="w-full">
+                <CardHeader>
+                    <div className="flex flex-row items-center gap-2 mt-4">
+                        <Avatar className="h-24 w-24 rounded-full mr-2">
+                            <AvatarImage src="/doofus-rick.png" alt="CN"/>
+                            <AvatarFallback/>
+                        </Avatar>
+                        <div className="flex flex-col">
+                            <div className="text-2xl font-bold">doofus rick</div>
+                            <div className="italic">
+                                cause it can't get any worse than this
+                            </div>
+                        </div>
+                    </div>
+                    <CardTitle className="mt-4 text-lg md:text-xl">
+                        Sign In
+                    </CardTitle>
+                    <CardDescription className="text-xs md:text-sm">
+                        Use your Discord account to login
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <div className="grid gap-4">
+                        <div
+                            className={cn(
+                                'w-full gap-2 flex items-center',
+                                'justify-between flex-col',
+                            )}
+                        >
+                            <SignInButton/>
+                        </div>
+                    </div>
+                </CardContent>
+                <CardFooter>
+                    <div className="flex justify-center w-full border-t py-4">
+                        <p className="text-center text-xs text-neutral-500">
+                            built by{' '}
+                            <Link
+                                href="https://konga.dev"
+                                className="underline"
+                                target="_blank"
+                            >
 							<span className="dark:text-white/70 cursor-pointer">
 								konga.
 							</span>
-						</Link>
-					</p>
-				</div>
-			</CardFooter>
-		</Card>
-	)
+                            </Link>
+                        </p>
+                    </div>
+                </CardFooter>
+            </Card>
+        </>
+    )
 }
